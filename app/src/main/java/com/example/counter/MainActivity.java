5e0d3340fd754ae985c3ea7d05b8504193e8629a
NimraHaq <nimrahaq10@gmail.com>
2022-05-04 11:53:55 +0500

    First commit

---
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.counter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int Count;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Count =0;
+    }
+
+    public void increaseCount(View view)
+    {
+        Count++;
+        TextView countValue = findViewById(R.id.count);
+        countValue.setText( Integer.toString(Count));
+
+    }
+    public void decreaseCount(View view)
+    {
+        Count--;
+        TextView countValue2 = findViewById(R.id.count);
+        countValue2.setText(Integer.toString(Count));
+
+    }
+}
